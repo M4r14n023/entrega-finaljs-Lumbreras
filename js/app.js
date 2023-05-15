f3e5aba87ca20515fe5aef0337e22b8700d915ff
Mariano Lumbreras <marianolumbreras@gmail.com>
2023-05-15 00:19:36 -0300

    Entrega final v1.1

--- js/app.js
+++ js/app.js
@@ -1,131 +1,10 @@
-// Obtener elementos del DOM
-const loginForm = document.querySelector("#login-form");
-const registerForm = document.querySelector("#register-form");
-const welcomeMessage = document.querySelector("h1");
-
-// // Manejar eventos de formulario de inicio de sesión
-// loginForm.addEventListener("submit", (event) => {
-//   event.preventDefault();
-//   const username = event.target.username.value;
-//   const password = event.target.password.value;
-//   const storedUser = localStorage.getItem(username);
-//   if (storedUser !== null && JSON.parse(storedUser).password === password) {
-//     window.location = 'pelisyseries.html';
-//     welcomeMessage.textContent = "Bienvenido " + username;
-//     const Toast = Swal.mixin({
-//       toast: true,
-//       position: 'top-end',
-//       showConfirmButton: false,
-//       timer: 2000,
-//       timerProgressBar: true,
-//       didOpen: (toast) => {
-//         toast.addEventListener('mouseenter', Swal.stopTimer)
-//         toast.addEventListener('mouseleave', Swal.resumeTimer)
-//       }
-//     })
-    
-//     Toast.fire({
-//       icon: 'success',
-//       title: 'Haz iniciado tu sesión'
-//     })
-//   } else {
-//     // alert("Nombre de usuario o contraseña incorrectos.");
-//     const Toast = Swal.mixin({
-//       toast: true,
-//       position: 'top-end',
-//       showConfirmButton: false,
-//       timer: 2000,
-//       timerProgressBar: true,
-//       didOpen: (toast) => {
-//         toast.addEventListener('mouseenter', Swal.stopTimer)
-//         toast.addEventListener('mouseleave', Swal.resumeTimer)
-//       }
-//     })
-    
-//     Toast.fire({
-//       icon: 'error',
-//       title: 'Nombre de usuario o contraseña incorrectos.'
-//     })
-//   }
-// });
-
-// // Manejar eventos de formulario de registro
-// registerForm.addEventListener("submit", (event) => {
-//   event.preventDefault();
-//   const newUsername = event.target["new-username"].value;
-//   const newPassword = event.target["new-password"].value;
-//   // const newUser = { password: newPassword };
-//   const newUser = { username: newUsername,password: newPassword };
-//   if (localStorage.getItem(newUsername) !== null) {
-//     const Toast = Swal.mixin({
-//       toast: true,
-//       position: 'top-end',
-//       showConfirmButton: false,
-//       timer: 2000,
-//       timerProgressBar: true,
-//       didOpen: (toast) => {
-//         toast.addEventListener('mouseenter', Swal.stopTimer)
-//         toast.addEventListener('mouseleave', Swal.resumeTimer)
-//       }
-//     })
-    
-//     Toast.fire({
-//       icon: 'warning',
-//       title: 'El usuario ya existe'
-//     });
-//   } else {
-    
-//     localStorage.setItem(newUsername, JSON.stringify(newUser));
-//     const Toast = Swal.mixin({
-//       toast: true,
-//       position: 'top-end',
-//       showConfirmButton: false,
-//       timer: 2000,
-//       timerProgressBar: true,
-//       didOpen: (toast) => {
-//         toast.addEventListener('mouseenter', Swal.stopTimer)
-//         toast.addEventListener('mouseleave', Swal.resumeTimer)
-//       }
-//     })
-    
-//     Toast.fire({
-//       icon: 'success',
-//       title: 'Usuario Registrado con Exito'
-//     })
-//   }
-// });
-
 // Obtener elementos HTML
-// const loginForm = document.querySelector("#login-form");
-// const registerForm = document.querySelector("#register-form");
 const moviesForm = document.querySelector("#movies-section form");
 const seriesForm = document.querySelector("#series-section form");
 const totalTimeSection = document.querySelector("#total-time");
 
 // Variables
 let totalTime = 0;
-
-// Eventos
-loginForm.addEventListener("submit", login);
-registerForm.addEventListener("submit", register);
-
-// Funciones
-function login(event) {
-  event.preventDefault();
-  // Obtener credenciales de localStorage y verificar si son correctas
-  // Actualizar mensaje de bienvenida con el nombre del usuario si es correcto
-}
-
-function register(event) {
-  event.preventDefault();
-  const newUsername = document.querySelector("#new-username").value;
-  const newPassword = document.querySelector("#new-password").value;
-  // Verificar si el nombre de usuario ya está en uso
-  // Si no lo está, almacenar credenciales en localStorage
-}
-
-
-
 
 // Obtenemos el formulario y los campos de entrada
 const formulario = document.getElementById("formulario");
@@ -136,7 +15,6 @@ const cantidadTemporadasInput = document.getElementById("cantidadTemporadas");
 const cantidadEpisodiosInput = document.getElementById("cantidadEpisodios");
 const duracionInput = document.getElementById("duracion");
 const datosSerieDiv = document.getElementById("datosSerie");
-// const botonpeli = document.getElementById("btnPeli");
 const labelDurPeli = document.getElementById("duracionPeliLabel")
 
 // Agregamos un evento al selector de tipo para mostrar o ocultar los campos de datos de serie
@@ -148,7 +26,6 @@ tipoSelect.addEventListener("change", () => {
     datosSerieDiv.style.display = "block";
     duracionInputP.style.display = "none";
     duracionInputP.value = duracionPelis;
-    // botonpeli.style.display = "none";
     labelDurPeli.style.display ="none"
   } else {
     datosSerieDiv.style.display = "none";
@@ -221,9 +98,7 @@ function mostrarLista() {
       }
       lista.appendChild(entradaLista);
     });
-// alert(`La duración total de las series es de ${duracionTotalSeries} minutos.`);
 document.getElementById("duracionSeries").textContent = `Duración total de las Series: ${duracionTotalSeries} minutos`;
-// alert(`La duración total de las películas es de ${duracionTotalPelis} minutos.`);
 document.getElementById("duracionPeliculas").textContent = `Duración total de las películas: ${duracionTotalPelis} minutos`;
   }
 
